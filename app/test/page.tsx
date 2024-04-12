f1ea7c95d98d0f53d6e21af2aecedd77868a4282
RentonMcintyre <rentonmcintyre@yahoo.com>
2024-04-12 14:32:07 +0100

    fix silly build error

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,7 +2,7 @@ export default function Test() {
   return (
     <section className="h-screen flex justify-center items-center">
       <button className="animate-spin btn btn-primary">
-        Woah, you're kind of silly, huh?
+        Woah, you&rsquo;re kind of silly, huh?
       </button>
     </section>
   );
