76f7163c67d1d3c25907ba657126f75caf9ff3bd
RentonMcIntyre <rentonmcintyre@yahoo.com>
2024-04-12 14:34:53 +0100

    Feature/setup (#5)
    * Initial setup - figuring out react and my available component library options
    
    * Fixed up select component
    
    * fix silly build error
    
    ---------
    
    Co-authored-by: Serencognito <serenmcintyre@gmail.com>

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+export default function Test() {
+  return (
+    <section className="h-screen flex justify-center items-center">
+      <button className="animate-spin btn btn-primary">
+        Woah, you&rsquo;re kind of silly, huh?
+      </button>
+    </section>
+  );
+}
