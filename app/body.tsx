e7ad6c973d3f8fa986035aebe0857d8dac6cb22c
RentonMcintyre <rentonmcintyre@yahoo.com>
2024-04-19 12:00:38 +0100

    Working theme switch using Menu component

--- app/body.tsx
+++ app/body.tsx
@@ -3,37 +3,28 @@
 import { useState } from "react";
 import { ChevronDown } from "lucide-react";
 import { cn } from "@/lib/utils";
-import Select, { SelectOption } from "@/components/ui/select";
+import Menu, { MenuOption } from "@/components/ui/menu";
 
 export default function Body(props: { children: any; className: string }) {
-  let [theme, setTheme] = useState("coffee");
-  const options: SelectOption[] = [
+  const options: MenuOption[] = [
     { value: "light", label: "Light" },
     { value: "dark", label: "Dark" },
-    { value: "coffee", label: "Coffee", group: "Fun" },
-    { value: "retro", label: "Retro", group: "Fun" },
+    { value: "coffee", label: "Coffee" },
+    { value: "retro", label: "Retro" },
   ];
 
+  let [theme, setTheme] = useState(options[3]);
+
   return (
-    <body className={cn(props.className, "flex-1")} data-theme={theme}>
-      <nav className="flex justify-end bg-primary">
-        <Select options={options} />
-        <div className="dropdown mx-4">
-          <div tabIndex={0} role="button" className="btn btn-primary">
-            Theme <ChevronDown />
-          </div>
-          <ul
-            tabIndex={0}
-            className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Item 2</a>
-            </li>
-          </ul>
-        </div>
+    <body className={cn(props.className, "flex-1")} data-theme={theme.value}>
+      <nav className="flex justify-end bg-primary px-5 py-1">
+        <Menu
+          anchor="end"
+          options={options}
+          variant="secondary"
+          callback={(option) => setTheme(option)}
+          label={"Theme: " + theme.label}
+        />
       </nav>
 
       <main>{props.children}</main>
--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,5 @@
 "use client";
-import Select, { SelectOption } from "@/components/ui/select";
-import Image from "next/image";
-import { ChevronDown } from "lucide-react";
 
 export default function Home() {
-  const options: SelectOption[] = [
-    { value: "light", label: "Light" },
-    { value: "dark", label: "Dark" },
-    { value: "coffee", label: "Coffee", group: "Fun" },
-    { value: "retro", label: "Retro", group: "Fun" },
-  ];
-
-  return (
-    <div className="flex items-center justify-center">
-      <Select options={options} />
-      <div className="dropdown mx-4">
-        <div tabIndex={0} role="button" className="btn btn-primary">
-          Theme <ChevronDown />
-        </div>
-        <ul
-          tabIndex={0}
-          className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-        >
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <a>Item 2</a>
-          </li>
-        </ul>
-      </div>
-
-
-    </div>
-  );
+  return <main></main>;
 }
