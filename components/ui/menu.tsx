c0d410b8a530518738893c62672dff4a23009f22
RentonMcintyre <rentonmcintyre@yahoo.com>
2024-04-19 12:00:38 +0100

    Working theme switch using Menu component

--- components/ui/menu.tsx
+++ components/ui/menu.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { Menu } from "@headlessui/react";
 import { useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 export interface MenuOption {
   value: string;
@@ -11,36 +14,42 @@ export interface MenuComponentProps {
   label?: string;
   anchor?: "start" | "end";
   position?: "top" | "bottom";
+  variant?: "primary" | "secondary";
   value?: string;
+  callback?: (option: MenuOption) => void;
 }
 
 const MenuComponent = (props: MenuComponentProps) => {
-  let classes = "dropdown";
-  if (props.anchor) {
-    classes += ` dropdown-${props.anchor}`;
-  }
-  if (props.position) {
-    classes += ` dropdown-${props.position}`;
-  }
+  const menuClasses = twMerge(
+    "dropdown",
+    props.anchor ? `dropdown-${props.anchor}` : "",
+    props.position ? `dropdown-${props.position}` : ""
+  );
 
-  let [value, setValue] = useState<string | undefined>(props.value);
+  const buttonClasses = twMerge(
+    "btn p-2 h-8 min-h-0",
+    props.variant ? `btn-${props.variant}` : ""
+  );
+
+  const selectItem = (option: MenuOption) => {
+    props.callback?.(option);
+  };
 
   return (
-    <div className={classes}>
+    <div className={menuClasses}>
       <Menu>
-        <Menu.Button className="btn btn-primary">
-          {value || props.label || "Menu"}
+        <Menu.Button className={buttonClasses}>
+          {props.label || "Menu"}
         </Menu.Button>
-        <Menu.Items className="dropdown-content z-[1] menu p-2 shadow bg-base-100 text-base-foreground rounded-box w-52">
+        <Menu.Items className="dropdown-content border-primary border-2 z-[1] menu p-2 shadow bg-base-100 text-base-foreground rounded-box w-52">
           {props.options.map((option) => (
             <Menu.Item
               key={option.value}
               as="a"
+              onClick={() => selectItem(option)}
               className="hover:bg-primary rounded-2xl hover:text-primary-foreground p-2"
             >
-              <button onClick={() => setValue(option.value)}>
-                {option.label}
-              </button>
+              {option.label}
             </Menu.Item>
           ))}
         </Menu.Items>
