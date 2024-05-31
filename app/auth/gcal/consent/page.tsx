dd7857b4e9d80f41f2f85009fc96998758591539
Renton McIntyre <rentonmcintyre@yahoo.com>
2024-05-31 14:38:51 +0100

    Use google cal service in consent screen

--- app/auth/gcal/consent/page.tsx
+++ app/auth/gcal/consent/page.tsx
@@ -1,13 +1,19 @@
-'use client'
-
 import { useSearchParams } from 'next/navigation';
 import { setGcalCode } from "@/lib/stores/auth-slice";
 import { useDispatch } from 'react-redux';
+import { redirect } from "next/navigation"
+import { GoogleCalendar } from "@/lib/service/calendar/google"; 
 
 export default function Consent() {
   const searchParams = useSearchParams();
+  const intent = searchParams.get('init');
   const code = searchParams.get('code');
   const dispatch = useDispatch();
+  const googleCalendarService = new GoogleCalendar();
+
+  if (init) {
+    console.debug(googleCalendarService.authUrl);
+  }
 
   if (code) {
     dispatch(setGcalCode(code));
